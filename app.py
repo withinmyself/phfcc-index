bf81fc78dd50c56ecca7966b64bf24e583c6f0cf
E.T. Deubner <withinmyself@gmail.com>
2018-03-11 14:23:19 -0500

    Update templates referenced.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return "Hello World!"
+def phfcc():
+    return render_template('phfcc.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/calendar')
+def calendar():
+    return render_template('calendar.html')
 
 if __name__ == '__main__':
     app.run()
